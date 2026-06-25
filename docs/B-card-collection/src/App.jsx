46a0ec310080bf3dc898d2d5991ebffa06a5afad
Arzik1987 <48416399+Arzik1987@users.noreply.github.com>
2026-06-25 10:54:03 +0000

    Add D3 to assignment B

--- docs/B-card-collection/src/App.jsx
+++ docs/B-card-collection/src/App.jsx
@@ -4,6 +4,11 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import * as d3 from "d3";
+
+const data = [4, 8, 15, 16, 23, 42];
+console.log("Max:", d3.max(data));
+
 function App() {
   const [count, setCount] = useState(0)
 
