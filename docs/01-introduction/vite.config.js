b4fd8b9e5a168031e9fa2df527e4e0b415e0b4c4
Arzik1987 <48416399+Arzik1987@users.noreply.github.com>
2026-07-07 20:25:39 +0000

    Fix 01-introduction GitHub Pages base path

--- docs/01-introduction/vite.config.js
+++ docs/01-introduction/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-d3-course/01-introduction/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
